1414714a9afddc9367b2ad02b266e7be59ee400e
Satyam Giri <satyamgiri@cpe-2606-a000-ee50-cd00-1061-7b1d-ca3a-9211.dyn6.twc.com>
2016-12-26 20:59:21 -0500

    Initial Commit

---
+++ Simple Animal Guessing Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Animal Guessing Game
+//
+//  Created by Satyam  Giri on 12/26/16.
+//  Copyright © 2016 Beginnersapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
